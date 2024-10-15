63c3bddd3c954afe117c53b99024d88733c765d9
cassandra.cavazos <cassandra.cavazos@coinmetrics.io>
2024-10-15 17:12:45 -0500

    create about page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import LandingPage from "./LandingPage"
+import AboutPage from "./AboutPage"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </Router>
   )
--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import "./LandingPage.css"
 
 const LandingPage = () => {
@@ -9,8 +10,8 @@ const LandingPage = () => {
           <h1>Swapped</h1>
         </div>
         <ul className="nav-links">
-          <li><a href="#about">About</a></li>
-          <li><a href="#features">Features</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
         <div className="nav-buttons">
