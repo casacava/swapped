98662e95aed8006006cd268d42dd6bfcc356c92c
casacava <cassiopeiacavazos@gmail.com>
2024-12-17 09:28:10 -0600

    install cors, fix API error

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 const router = express.Router()
 
-// sign up
+// sign up router
 router.post('/api/auth/signup', async (req, res) => {
   const { name, email, password, bio, location } = req.body
 
@@ -29,6 +30,24 @@ router.post('/api/auth/signup', async (req, res) => {
 } catch (error) {
   res.status(500).json({ message: 'server error', error})
 }
+})
+
+// Sign-in route
+router.post('/api/auth/signin', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email }); // Fetch user from database
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.status(401).json({ message: 'Invalid credentials' });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET || 'your_jwt_secret', { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -4,38 +4,21 @@ const express = require('express')
 const mongoose = require ('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const app = express()
-const PORT = process.env.PORT || 3000
+const cors = require('cors')
 const path = require('path')
 const User = require('./models/User')
-// const router = express.Router
 const authRoutes = require('./routes/authRoutes')
+
+const app = express()
+const PORT = process.env.PORT || 3000
 
 // middleware
 app.use(express.json()) // to parse JSON requests
-
-// Sign-in route
-app.post('/api/auth/signin', async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email }); // Fetch user from database
-    if (!user) return res.status(404).json({ message: 'User not found' });
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid)
-      return res.status(401).json({ message: 'Invalid credentials' });
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET || 'your_jwt_secret', { expiresIn: '1h' });
-    res.status(200).json({ token });
-  } catch (error) {
-    res.status(500).json({ message: 'Server error' });
-  }
-})
+app.use(cors())
 
 // Sign-Up Route
 app.use(authRoutes)
 
-module.exports = router
 
 // connect to MongoDB
 const dbURI = process.env.MONGO_URI || 'mongodb+srv://cassiopeiacavazos:XQxtEBiTgMSPCuWv@swappedcluster.ibrp7.mongodb.net/?retryWrites=true&w=majority&appName=SwappedCluster';
